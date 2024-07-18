bc0bfca31d2f4cf28448fa4a8d92a2046ef5d966
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-07-18 14:23:46 -0700

    parsing and converting variables and data types

---
+++ app2.js
@@ -0,0 +1,28 @@
+let myStr = "Be-Asia";
+let myNum = 0;
+let a;
+let b, c, d; 
+let val;
+
+//get the val
+
+myStr = 5; 
+myNum = 'test';
+
+b = 5;
+c = 10
+d = Number('20'); //don't forget wrapping with string or number
+d = null; 
+d = c.toString();
+a = '1000x';
+d = parseInt(a); //turn it back into a num
+
+
+val = typeof (myNum)
+val = b + c + d; //javascript will convert everything to a string in this case
+val = 100 + d; 
+
+console.log(val)
+document.write(val);
+
+//convert
