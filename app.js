d4572ac9fcddeedb69381132624cce1774d9c985
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-07-18 12:32:15 -0700

    working with basic html elements

---
+++ app.js
@@ -0,0 +1,4 @@
+document.write("<div> Hello World 3 <div>");
+document.write("<div> Hello World 4 <div>");
+document.write("<div> Hello World 5 <div>");
+document.write("<div> Hello World 6 <div>");
