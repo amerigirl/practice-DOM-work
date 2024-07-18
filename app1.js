defe1a60fca44800deb9547ae83090d5bebb1714
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-07-18 14:08:23 -0700

    enjoying string manip and concatenation

--- app1.js
+++ app1.js
@@ -8,7 +8,17 @@ console.log(myName);
 {
     const b = 1000;
     console.log(b);
+    var c = 100; 
+    let d = 1001
 }
-
+//console.log(d); // creates a scope error!
 let val = a;
 document.write(a)
+
+//fun with string creation :)
+
+let myStr1 = 'Hello\'s world';  //add your comma with a backslash
+console.log(myStr1);
+let myStr2 = '\"Hello\" world';
+console.log(myStr2) //add your inner quotes
+let templit = ``;
