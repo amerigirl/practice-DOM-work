624d9d2573f417cac879e218b6e1837397ca1c5b
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-07-21 15:22:00 -0700

    working with the practice DOM elements test

--- dom1.js
+++ dom1.js
@@ -1,13 +1,13 @@
 //get the text content from the DOM
 
 const ele = document.body.children[0]; //(took .textContent off in order to get the element not just the words--the div box)
-console.log(ele);
+//console.log(ele);
 
 //update the text content
 let val = "Be-Asia McKerracher"
 document.body.children[0].textContent = val;
 ele.textContent = 'updated'  //keeping it as an object allows you to update without changing the variable name
 
-console.log(ele.className)
-console.log(ele.innerHTML)
+//console.log(ele.className)
+//console.log(ele.innerHTML)
 ele.innerHTML = '<h1>Hello</h1>'
--- quizCode.js
+++ quizCode.js
@@ -1,40 +1,27 @@
 //  Question 1: Select an element with the id "header"
 
-
-
 // *Question 2: Add a new class to an element
-
 
 //* Question 3: Check if a div with the id "content" is empty
 
-
-
-
 // Question 4: Remove an element from the DOM
-
-
-
 
 //* Question 5: Change the background color of the element with id "content" to green
 
-
 //console.log(content);
-
 
 // Question 6: Add a role attribute with the value "button" to a div with the id "myDiv"
 
-
-
 // Question 7: Append a new child element to an existing element
-
 
 // Question 8: Remove all paragraph elements from the document
 
-
-
 //* Question 9: Change the text content of an element
 
-
-
-
 // Question 10: Create a new div element
+
+// Question 11: Add a click event listener to the button that changes the color of the div with id "colorBox" to blue
+
+// Question 12: Add a mouseover event listener to the div with id "hoverBox" that changes its text content to "Mouse is over me!"
+
+  // Question 13: Add a keydown event listener to the input element that displays the current input value in the paragraph with id "output"
